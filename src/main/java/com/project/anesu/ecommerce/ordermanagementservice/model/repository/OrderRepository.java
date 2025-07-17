b561346d4d4eeade27fecc83e05fe38b06b03b9a
anesukuveya <anesugail16@gmail.com>
2025-07-17 17:16:03 +0200

    Add data access layer

---
+++ src/main/java/com/project/anesu/ecommerce/ordermanagementservice/model/repository/OrderRepository.java
@@ -0,0 +1,12 @@
+package com.project.anesu.ecommerce.ordermanagementservice.model.repository;
+
+import com.project.anesu.ecommerce.ordermanagementservice.entity.order.Order;
+import com.project.anesu.ecommerce.ordermanagementservice.entity.order.OrderStatus;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+  Optional<Order> findByIdAndOrderStatus(Long orderId, OrderStatus status);
+}
