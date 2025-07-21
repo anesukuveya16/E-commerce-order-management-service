3ceb97c211c8e13264dd67a094090e1e9fc20976
anesukuveya <anesugail16@gmail.com>
2025-07-21 10:00:43 +0200

    Delete AddressRepository as it's no longer needed

--- src/main/java/com/project/anesu/ecommerce/ordermanagementservice/model/repository/AddressRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.anesu.ecommerce.ordermanagementservice.model.repository;
-
-import com.project.anesu.ecommerce.ordermanagementservice.entity.address.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepository extends JpaRepository<Address, Long> {}
